f40a7da0fc60315189e0ca335925542afaf4c2f9
Jason Hirata <jhirata@integrity-apps.com>
2017-10-29 16:00:58 -1000

    added card store

---
+++ src/card_store.js
@@ -0,0 +1,60 @@
+import Http from 'utils';
+import { Observable } from 'rxjs'
+
+export default (function() {
+
+    let mapper = card_id => {
+	return Observable.fromPromise(Http({method:"GET",url:"/api/cardmapping/mapping"}))
+	    .map(JSON.parse)
+	    .map(({mapping}) => {
+		//		    let db = mapping.find(({prefix}) => card_id.startsWith(prefix));
+		let matching_dbs = mapping.filter( ({ prefix }) => card_id.startsWith(prefix));
+		let max = -1
+		let max_index = -1;
+		
+		matching_dbs.forEach(({prefix},j) => {
+		    if(prefix.length > max) {
+			max = prefix.length;
+			max_index = j;
+		    }
+		})
+		return matching_dbs[max_index];
+	    })
+
+    }
+    
+    let cardmapper = card_id => {
+	return mapper(card_id)
+	    .mergeMap(db => Observable.fromPromise(Http({method:"GET",url:"/api/" + db.db + "/" + card_id})).map(JSON.parse).map(obj => Object.assign({},obj, {id:obj._id})));
+	
+    }
+    
+    return {
+
+
+	// get a card based on id
+	getcard(card_id) {
+	    return cardmapper(card_id);
+	},
+
+	// get all cards from a card set id
+	getcardsfromset(id) {
+	    //	    if(id === 'VS') {
+	    //		return Rx.Observable.from(testing);
+	    //	    }
+	    //	    selecteddb = id;
+	    return Observable.fromPromise(Http({method:"GET",url:"/api/" + id + "/_design/view/_list/all/all"}))
+		.map(JSON.parse)
+		.mergeMap(data => {
+		    return Observable.from(data)
+		})
+		.map(obj => Object.assign({},obj,{id:obj._id}));
+	},
+
+	// get all known card sets
+	getcardsets() {
+	    //	    return Rx.Observable.from([{id:"VS",label:"Vivid Strke"}]).toArray();
+	    return Observable.fromPromise(Http({method:"GET",url:"/api/cardsets/sets"})).map(JSON.parse).pluck('sets');
+	}
+    };
+})()
